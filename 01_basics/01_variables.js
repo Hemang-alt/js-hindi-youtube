7add323b681d66f3972f366d366a4c1b6c43f6d1
Hemang_K_Nanda <knandahemang05@gmail.com>
2025-12-31 18:25:16 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hemangknanda@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+accountEmail = "hahaha"
+accountPassword = "2343434"
+// console.log(accountId);
+console.log([accountEmail,accountId,accountPassword,accountState]);
+
+// var is not used in coding now 
