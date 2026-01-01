c99999cf77a4f9ba30d6d265839ddd761adcef7e
Hemang_K_Nanda <knandahemang05@gmail.com>
2026-01-01 11:20:04 +0000

    conversion of datatypes

---
+++ 01_basics/03_dataConversion.js
@@ -0,0 +1,21 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// NaN = not a number
+
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber)
