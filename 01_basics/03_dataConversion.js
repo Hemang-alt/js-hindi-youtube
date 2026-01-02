a86695aae2bfc8189f40030d7acc1f70cb02c104
Hemang_K_Nanda <knandahemang05@gmail.com>
2026-01-02 08:00:45 +0000

    datatype conversion

--- 01_basics/03_dataConversion.js
+++ 01_basics/03_dataConversion.js
@@ -18,4 +18,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// operations //
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " hemang"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 1); //if string is at first then it converts all to string i.e 121
+console.log(1 + 2 + "1"); // otherwise it will do this to 31
+
+console.log(+true);
+
---
+++ 01_basics/04_dataComparison.js
@@ -0,0 +1,4 @@
+// strict check ===
+
+console.log("2" == 2);
+console.log("2" === 2);
