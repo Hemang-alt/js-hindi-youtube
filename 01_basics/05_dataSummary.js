9197c493d199260a653ce2274bca198fe7693f4e
Hemang_K_Nanda <knandahemang05@gmail.com>
2026-01-02 08:34:05 +0000

    memory storage

--- 01_basics/05_dataSummary.js
+++ 01_basics/05_dataSummary.js
@@ -1,1 +1,22 @@
 // primitive and non primitve two types are there
+
+
+
+// Memory - stack(primitive) and heap(non primitive)
+let one = "hemang"
+let two = one;
+two = "knanda"
+console.log(one);
+console.log(two);
+
+let userone = {
+    email : "hahaha",
+    id : "1"
+};
+let usertwo = userone;
+
+usertwo.email = "heheheee";
+
+console.log(usertwo.email);
+console.log(userone.email);
+
