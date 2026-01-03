b5e3f272ab46bbaea82052fb77e307f5feb3d183
Hemang_K_Nanda <knandahemang05@gmail.com>
2026-01-03 12:11:06 +0000

    arrays one

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// Arrays
+
+const myArr = [0,1,2,3,4,5];
+
+const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[2]);
+
+
+// array methods
+
+
+// myArr.push(6);
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+
+myArr.unshift(9);
+// console.log(myArr);
+myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(4));
+
+
+// const newArr = myArr.join();
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice , splice
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B" , myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+console.log("C" , myArr);
