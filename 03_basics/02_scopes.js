dc60734b1e0cb2ba09f6092170b9049898d34fcf
Hemang_K_Nanda <knandahemang05@gmail.com>
2026-01-10 05:44:05 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,37 @@
+var c = 300;
+let a = 300;
+
+if(true){
+    let a = 10;
+    const b = 20;
+    // console.log("INNER :",a);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Hemang";
+
+    function two(){
+        const website = "Youtube";
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+addOne(5);
+
+function addOne(num){
+    return num+1;
+}
+
+addTwo(5);
+const addTwo = function(num){
+    return num + 2;
+}
