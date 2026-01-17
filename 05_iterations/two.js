94064366810527d32ac91d299c553fe76f803d2a
Hemang_K_Nanda <knandahemang05@gmail.com>
2026-01-17 13:17:56 +0000

    do while and while

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let index = 0;
+while(index <= 10){
+    // console.log(`values of index is ${index}`);
+    index = index + 2;
+}
+
+let myarr = ["flash","batman","superman"];
+let arr = 0;
+while(arr<arr.length){
+    console.log(`Value is ${myarr[arr]}`);
+    arr = arr + 1;
+}
+
+let score = 11;
+do{
+    console.log(`score is ${score}`);
+    score++;
+} while(score<=10);
